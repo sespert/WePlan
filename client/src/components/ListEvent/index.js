abdac1a79dd15e5d254187b65a0e2ee1b34fede0
Lady Diana <dianapachon2017@gmail.com>
2019-05-14 21:27:39 -0400

    css

--- client/src/components/ListEvent/index.js
+++ client/src/components/ListEvent/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./style.css";
+import "./listEvent.css";
 import { Container, Row, Col } from "../Grid";
 
 // This file exports both the List and ListItem components
 
 export function List({ children }) {
+  console.log(children);
   return (
-    <div className="list-overflow-container">
-      <ul className="list-group">{children}</ul>
+    <div className="container3" >
+      <ul className="list-group">{children} </ul>
     </div>
   );
 }
