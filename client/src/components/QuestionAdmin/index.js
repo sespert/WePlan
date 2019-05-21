d98d4bb9952a7e960538fca6099a76b3045bcb15
sespertg <sespert@gmail.com>
2019-05-21 13:54:20 -0400

    data validation for event creation

--- client/src/components/QuestionAdmin/index.js
+++ client/src/components/QuestionAdmin/index.js
@@ -1,8 +1,10 @@
 import React, { Component }  from "react";
 import { Redirect } from "react-router-dom";
 import { Input, FormBtn, TextArea } from "../Form";
+import FormErrors from "../FormErrors";
 import "./style.css";
 import API from "../../utils/API";
+import moment from "moment";
 
 
 class QuestionAdmin extends Component {
@@ -18,14 +20,79 @@ class QuestionAdmin extends Component {
             startTime: "",
             endTime: "",
             referrer: null,
-            userId: null
+            userId: null,
+            formErrors: {name: "", place: "", date: "", numOfDays: "", startTime: "",
+            endTime: ""},
+            nameValid: false,
+            placeValid: false,
+            dateValid: false,
+            numOfDaysValid: false,
+            startTimeValid: false,
+            endTimeValid: false,
+            formValid: false
         };
     }
 
        
     handleChange = e => {
-        e.preventDefault();
-        this.setState({[e.target.name] :  e.target.value})
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState(
+            {[name] : value},
+            () => { this.validateField(name, value) }
+        );
+    }
+
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let nameValid = this.state.nameValid;
+        let placeValid = this.state.placeValid;
+        let dateValid = this.state.dateValid;
+        let numOfDaysValid = this.state.numOfDaysValid;
+        let startTimeValid = this.state.startTimeValid;
+        let endTimeValid = this.state.endTimeValid;
+
+
+        switch(fieldName) {
+            case 'name':
+                nameValid = value.length > 0;
+                fieldValidationErrors.name = nameValid ? '' : ' is empty';
+                break;
+            case 'place':
+                placeValid = value.length > 0;
+                fieldValidationErrors.place = placeValid ? '' : ' is empty';
+                break;
+            case 'date':
+                dateValid = moment(value, 'MM/DD/YYYY',true).isValid();
+                fieldValidationErrors.date = dateValid ? '' : ' is invalid';
+                break;
+            case 'numOfDays':
+                numOfDaysValid = (!/\D/.test(value));
+                fieldValidationErrors.numOfDays = numOfDaysValid ? '' : ' is invalid';
+                break;
+            case 'startTime':
+                startTimeValid = moment(value, 'h:mm a', true).isValid();
+                fieldValidationErrors.startTime = startTimeValid ? '' : ' is invalid';
+                break;
+            case 'endTime':
+                endTimeValid = moment(value, 'h:mm a', true).isValid();
+                fieldValidationErrors.endTime = endTimeValid ? '' : ' is invalid';
+                break;
+            default:
+                break;
+        }
+        this.setState({formErrors: fieldValidationErrors,
+            nameValid: nameValid,
+            placeValid: placeValid,
+            dateValid: dateValid,
+            numOfDaysValid: numOfDaysValid,
+            startTimeValid: startTimeValid,
+            endTimeValid: endTimeValid
+          }, this.validateForm);
+    }
+
+    validateForm() {
+        this.setState({formValid: this.state.nameValid  && this.state.placeValid && this.state.dateValid && this.state.numOfDaysValid && this.state.startTimeValid && this.state.endTimeValid});
     }
 
     handleSubmit = e => {
@@ -57,6 +124,7 @@ class QuestionAdmin extends Component {
         return (
             
             <form>
+                <FormErrors formErrors={this.state.formErrors} />
                 <Input name="name" placeholder="Name of the event (required)" value={this.state.name} onChange={this.handleChange}/>
                 <Input name="place" placeholder="Place (required)" value={this.state.place} onChange={this.handleChange}/>
                 <TextArea name="subject" placeholder="Description of the event" value={this.state.subject} onChange={this.handleChange}/>
@@ -65,7 +133,8 @@ class QuestionAdmin extends Component {
                 <Input name="startTime" placeholder="Start time: 12:00 AM (required)" value={this.state.startTime} onChange={this.handleChange}/> 
                 <Input name="endTime" placeholder="End time: 12:00 AM (required)" value={this.state.endTime} onChange={this.handleChange}/> 
                 <h2>{this.props.userId}</h2>
-                <FormBtn onClick={this.handleSubmit}>Create Event</FormBtn>
+                <FormBtn onClick={this.handleSubmit} disabled={!this.state.formValid} >Create Event</FormBtn>
+                <FormErrors formErrors={this.state.formErrors} />
             </form>
       
         )
