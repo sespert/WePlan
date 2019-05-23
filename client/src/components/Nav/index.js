4baa393c5c11f50d45ec5d31e49eb1883aa3c4d6
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-05-22 23:50:38 -0400

    Nav slogan responsible

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,32 +5,32 @@ import logo from '../../images/logo.png'
 
 
 class Nav extends Component {
- 
+
   render() {
     let partial;
-    if(this.props.id) {
+    if (this.props.id) {
       partial = <li className="nav-item-2">
-        <a className="nav-link3" href="/">Log Out</a>          
-      </li>  
+        <a className="nav-link3" href="/">Log Out</a>
+      </li>
     } else {
       partial = <li className="nav-item-2">
-      <a className="nav-link3" href="/">Log In</a>          
-    </li>  
+        <a className="nav-link3" href="/">Log In</a>
+      </li>
 
     }
     return (
       <nav id="navBar" className="navbar navbar-expand-lg">
-          <a className="navbar-brand" >
-            <img src={logo} height="100" width="150" alt="logo"/> 
-          </a>
-          <div className="collapse navbar-collapse" id="navbarText">
+        <a className="navbar-brand" >
+          <img src={logo} height="100" width="150" alt="logo" />
+        </a>
+        <div className="collapse1 1navbar-collapse" id="navbarText">
           <span className="navbar-text" id="legend">
             Life can be easy by just using the right tools!
           </span>
-          </div>
+        </div>
       </nav>
     );
+  }
 }
-}
-    
+
 export default Nav;
